9e0b007632e5fd2f7c80cdd77413b7d867a62e0f
niranjan-gurung <gurung5130@gmail.com>
2026-03-01 17:43:34 +0000

    more svg added, svg alignment fixed, sidebar styling.

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,8 +1,22 @@
 <template>
-  <button>+ Create (drop down)</button>
-  <section>
-    mini calendar view (selector)
-  </section>
+  <div class="space-y-4">
+    <button class="flex items-center gap-2 bg-white
+            shadow-md/20 rounded-2xl
+            px-6 py-4 text-md font-semibold text-slate-800
+            hover:cursor-pointer hover:bg-blue-100
+            active:bg-blue-200 transition-colors duration-200 ease-out">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="size-4 shrink-0 translate-y-px">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+      </svg>
+      Create
+      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="0" stroke="currentColor" class="size-4 shrink-0 self-center translate-y-px">
+        <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+      </svg>
+    </button>
+    <section>
+      mini calendar view (selector)
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
